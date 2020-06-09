cc91036a104c73a197c868e0e27aae7e78952bc4
great084 <great084@gmail.com>
2020-06-09 11:28:34 +0900

    問題データの作成

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -3,6 +3,13 @@ class QuestionController < ApplicationController
   end
 
   def create
-    puts params
+    puts params[:number_of_questions]
+    # byebug
+    quiz_order = (0..46).to_a.shuffle.slice(0..(params[:number_of_questions].to_i)-1)
+    # byebug
+    puts "問題番号は#{quiz_order}です。問題数は#{quiz_order.count}です。"
+    @questions = Prefecture.where(id: quiz_order)
+    # byebug
+    render 'question/start'
   end
 end
