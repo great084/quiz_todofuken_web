77801c76965bb6d56d3c0bbf85659ef3af0b4a3e
great084 <great084@gmail.com>
2020-06-12 16:06:27 +0900

    rootの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'question#index'
   get 'question/index'
   post 'question/create', to: 'question#create'
   # get 'question/start'
