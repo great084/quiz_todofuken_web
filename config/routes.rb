b89ed138cb35128a4d2d2655893c335fff1367c4
great084 <great084@gmail.com>
2020-06-15 11:45:13 +0900

    不要なview,controller,routeを削除

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'quiz#new'
   post 'quiz/create'
-  get 'question/index'
-  post 'question/create', to: 'question#create'
-  # get 'question/start'
 end
